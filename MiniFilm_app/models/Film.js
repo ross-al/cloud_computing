6343f4f28c7e8e99e67ad3c783807e18d63fb353
ross-al <115371010+ross-al@users.noreply.github.com>
2023-10-13 19:54:29 +0100

    added specific MiniFilmApp collection to MongoDB URI

--- MiniFilm_app/models/Film.js
+++ MiniFilm_app/models/Film.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
-const FilmSchema = mongoose.Schema({
+const Schema = mongoose.Schema;
+
+const filmSchema = new Schema({
     film_name:{
         type:String
     },
@@ -15,4 +17,7 @@ const FilmSchema = mongoose.Schema({
     }
 })
 
-module.exports = mongoose.model('films',FilmSchema)
+module.exports = mongoose.model('Film',filmSchema); 
+//mongoose by default looks for lower case plural collections of Schema
+//e.g. if Film, then will look for collection called 'films'
+
